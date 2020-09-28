16159b4e3b0a371119d0e445b48278af57586d4f
Robert Vang <rob7v18@gmail.com>
2020-09-28 18:57:51 -0500

    second commit

--- server.js
+++ server.js
@@ -5,25 +5,23 @@ const cors = require('cors')
 const helmet = require('helmet')
 const MOVIE =require('./movies.json')
  
-console.log(process.env.API_TOKEN)
-
 const app = express()
  
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
-    console.log('validate bearer token middleware')
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      return res.status(401).json({ error: 'Unauthorized request' })
-    }
-    
-    // move to the next middleware
-    next()
-  })
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
  
 function handleGetMovie(req,res){
   let response = MOVIE;
@@ -46,14 +44,21 @@ function handleGetMovie(req,res){
   }
  
   res.json(response)
- 
 }
  
 app.get('/movie', handleGetMovie)
  
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+}) 
  
- 
-const PORT = 8000
+const PORT = process.env.PORT || 8000
  
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
